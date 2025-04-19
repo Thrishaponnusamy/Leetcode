32a7f5baee9f108d7c9bd23f75fd4f940988d62c
Thrishaponnusamy <thrishaponnusamy@gmail.com>
2025-04-19 09:55:05 +0530

    Time: 5 ms (71.24%), Space: 44.2 MB (47.95%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int dup=x;
+        int rev=0;
+        while(x>0){
+            int d=x%10;
+            rev=rev*10+d;
+            x=x/10;
+        }
+        if(dup==rev){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
