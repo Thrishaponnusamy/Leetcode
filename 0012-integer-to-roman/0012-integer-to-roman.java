cfe95d071656c3db19e2328af5a7de89699c65e1
Thrishaponnusamy <thrishaponnusamy@gmail.com>
2025-06-27 14:32:05 +0530

    Time: 5 ms (32.74%), Space: 44.2 MB (83.33%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -3,7 +3,7 @@ class Solution {
         int []n ={1000,900,500,400,100,90,50,40,10,9,5,4,1};
         String[] s = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
         int i =0;
-        String str = new String(); 
+        String str="";
         while (num>0){
             if (num>=n[i]){
                 str=str+s[i];
