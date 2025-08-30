16c05f085cbb0192f977f695aeed1d962a4a8375
Thrishaponnusamy <thrishaponnusamy@gmail.com>
2025-08-30 14:05:02 +0530

    Time: 1 ms (96.76%), Space: 44.7 MB (29.9%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.java
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.java
@@ -1,29 +1,25 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-        int n = nums.length;
-        int ops = 0;
-        int start = 0; // pointer to track the start of array
-
-        while (true) {
-            // Check if all elements from start to end are distinct
-            boolean distinct = true;
-            for (int i = start; i < n; i++) {
-                for (int j = i + 1; j < n; j++) {
-                    if (nums[i] == nums[j]) {
-                        distinct = false;
-                        break;
-                    }
-                }
-                if (!distinct) break;
-            }
-            if (distinct) break; // done if all distinct
-
-            // Remove first 3 elements by moving the start pointer
-            start += 3;
-            ops++;
-            if (start >= n) break; // array empty
+        int s=0;
+        int n=nums.length;
+        int o=0;
+        while(true){
+        boolean d=true;
+        for(int i=s;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]==nums[j]){
+                     d=false;
+                     break;
+                }}
+                if(!d) break;
         }
-
-        return ops;
+                if(d) break;
+                    s+=3;
+                    o++;
+                
+            
+            if (s >= n) break;
+        }
+  return o;
     }
 }
