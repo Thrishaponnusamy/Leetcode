6fb4cb277030a1d9972013feff75234199e0080e
Thrishaponnusamy <thrishaponnusamy@gmail.com>
2025-09-01 14:20:09 +0530

    Time: 1 ms (63.65%), Space: 41.6 MB (62.18%) - LeetHub

---
+++ 1797-goal-parser-interpretation/1797-goal-parser-interpretation.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String interpret(String command) {
+        String newStr = command.replace("()", "o"); 
+              newStr = newStr.replace("(al)", "al");
+        return newStr;
+    }
+}
