eee36dadc92e84fed6ac81d36f378fa761268d40
Thrishaponnusamy <thrishaponnusamy@gmail.com>
2025-09-02 23:16:58 +0530

    Time: 1 ms (52.74%), Space: 40.6 MB (89.38%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isUgly(int n) {
+         if (n <= 0) return false; 
+        if(n==1) return true;
+     int []pp={2,3,5};
+     for(int p:pp){
+        while(n%p==0){
+            n/=p;
+        }
+     }
+     return n==1;
+    }
+}
